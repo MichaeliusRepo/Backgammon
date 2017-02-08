2fa86afd0d0da6a48d6056996d052ca93a138613
Michael <Michael@DESKTOP-3EU71Q1>
2017-02-08 21:00:32 +0100

    Add project files.

---
+++ Backgammon/Screen/ScreenManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Backgammon.Screen
+{
+    public class ScreenManager
+    {
+        public Vector2 Dimension { private set; get; }
+        public ContentManager Content { private set; get; }
+        GameScreen currentScreen;
+
+        // Make a singleton class
+        private static ScreenManager instance;
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ScreenManager();
+                return instance;
+            }
+        }
+        private ScreenManager()
+        { // A private constructor overrides any default, public constructors.
+            Dimension = new Vector2(640, 480);
+            currentScreen = new SplashScreen();
+        }
+
+
+
+        public void LoadContent(ContentManager Content)
+        {
+            this.Content = new ContentManager(Content.ServiceProvider, "Content");
+            currentScreen.LoadContent();
+        }
+
+        public void UnloadContent()
+        {
+            this.Content.Unload();
+            currentScreen.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            currentScreen.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+        }
+
+    }
+}
---
+++ Backgammon/Screen/SplashScreen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Backgammon.Screen
+{
+    public class SplashScreen : GameScreen
+    {
+        Texture2D image;
+        string path;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            path = "Images/Backgammons";
+            image = content.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            //base.Draw(spriteBatch);
+            spriteBatch.Draw(image, Vector2.Zero, Color.White);
+        }
+
+
+    }
+}
