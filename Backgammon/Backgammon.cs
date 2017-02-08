1119e9154faa0b7f7a7250d42e2c6dc6ccd39229
MichaeliusDenModige <s143049@student.dtu.dk>
2017-02-08 21:31:33 +0100

    A second solution should exist.

--- Backgammon/Backgammon.cs
+++ Backgammon/Backgammon.cs
@@ -64,6 +64,7 @@ namespace Backgammon
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            // This method is called about 30 times per second, i.e. 30 FPS.
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
