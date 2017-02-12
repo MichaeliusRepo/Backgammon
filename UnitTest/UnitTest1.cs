f272c44d4acde3b9886734b585bb954aec4ae9a8
MichaeliusDenModige <s143049@student.dtu.dk>
2017-02-12 20:07:43 +0100

    Added unit test project to solution.

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // By default, unit testing assumes that exceptions are thrown when actions are invalid.
+        // CTRL R, A to run
+        // Alternatively, go to Tests > Run > All Tests
+
+        int one, two, three;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            one = 1;
+            two = 2;
+            three = 3;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+            Assert.IsFalse(false);
+            Assert.IsNull(null);
+
+            Assert.AreEqual(3, three);
+            Assert.AreNotEqual("not Expected", "actual value");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            try
+            {
+                int div = 1 / (one * 2 - two);
+                Assert.Fail();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
