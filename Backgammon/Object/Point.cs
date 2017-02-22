e539103aae309e42c3574e5657709b7a83897e1d
MichaeliusDenModige <s143049@student.dtu.dk>
2017-02-22 22:43:08 +0100

    Added a basic graphic example run.

---
+++ Backgammon/Object/Point.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backgammon.Screen;
+using Microsoft.Xna.Framework;
+using ModelDLL;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Backgammon.Object
+{
+
+    internal class Point
+    {
+        internal int Number { get; private set; }
+        internal Vector2 Position { get; private set; }
+        private List<Checker> Checkers;
+
+        // Modify this value for Y distance (if < 5) between checkers
+        private readonly static float checkerDistance = 56.0f;
+
+        internal Point(Vector2 Position, List<Checker> Checkers)
+        {
+            this.Position = Position;
+            this.Checkers = Checkers;
+            //foreach (Checker checker in Checkers)
+            //    if (Position.Y > 360) // Is this Point at bottom?
+            //        checker.SetPosition(Position.X, Position.Y - Checkers.Count * checkerDistance);
+            //    else // or on top?
+            //        checker.SetPosition(Position.X, Position.Y + Checkers.Count * checkerDistance);
+
+            for (int i = 0; i < Checkers.Count; i++)
+                if (Position.Y > 360) // Is this Point at bottom?
+                    Checkers[i].SetPosition(Position.X, Position.Y - i * checkerDistance);
+            else
+                    Checkers[i].SetPosition(Position.X, Position.Y + i * checkerDistance);
+        }
+
+        //internal Point(Vector2 Position)
+        //{
+        //    this.Position = Position;
+        //    Checkers = new List<Checker>();
+        //}
+
+        internal void AddChecker(Checker checker)
+        {
+            Checkers.Add(checker);
+            if (Checkers.Count < 15) // Do we need to make space for many checkers
+            {
+                if (Position.Y > 360) // Is this Point at bottom?
+                    checker.SetPosition(Position.X, Position.Y - Checkers.Count * checkerDistance);
+                else // or on top?
+                    checker.SetPosition(Position.X, Position.Y + Checkers.Count * checkerDistance);
+            }
+            else // Make space for many checkers
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+        internal void RemoveChecker(Checker checker)
+        {
+            Checkers.Remove(checker);
+        }
+
+        internal void Update(GameTime gameTime)
+        {
+            foreach (Checker c in Checkers)
+                c.Update(gameTime);
+        }
+
+        internal void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Checker c in Checkers)
+                c.Draw(spriteBatch);
+        }
+
+
+    }
+}
--- Backgammon/Screen/BoardScreen.cs
+++ Backgammon/Screen/BoardScreen.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Xml.Serialization;
-using Backgammon.Input;
+using System.Threading.Tasks;
 
+using Backgammon.Input;
+using Backgammon.Object;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Backgammon.Screen
 {
-    public class SplashScreen : GameScreen
+    public class BoardScreen : GameScreen
     {
-        public Image Image;
+        public Image Image = new Image { Path = "Images/Board", Position = new Vector2(540, 360)};
+        public Board Board;
 
         public override void LoadContent()
         {
             base.LoadContent();
             Image.LoadContent();
             //Image.FadeEffect.FadeSpeed = 0.5f;
+            Board = new Board();
         }
 
         public override void UnloadContent()
@@ -32,17 +35,17 @@ namespace Backgammon.Screen
         {
             base.Update(gameTime);
             Image.Update(gameTime);
+            Board.Update(gameTime);
 
-            if (InputManager.Instance.KeyPressed(Keys.Enter, Keys.Z))
-                //ScreenManager.Instance.ChangeScreens("SettingsScreen");
-                ScreenManager.Instance.ChangeScreens("BoardScreen");
+            // TODO Deal with inputs here
+            //if (InputManager.Instance.KeyPressed(Keys.Enter, Keys.Z))
+            //    ScreenManager.Instance.ChangeScreens("SettingsScreen");
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             Image.Draw(spriteBatch);
+            Board.Draw(spriteBatch);
         }
-
-
     }
 }
