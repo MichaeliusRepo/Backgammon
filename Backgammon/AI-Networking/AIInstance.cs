6353f3dc61f9ddc3656b2e0aade70cb267606549
MichaeliusDenModige <s143049@student.dtu.dk>
2017-06-09 12:35:17 +0200

    Implemented AIInstance and ported NaiveAI there. Replaced Option screen, made it work with AIInstance.

---
+++ Backgammon/AI-Networking/AIInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelDLL;
+
+namespace Backgammon.AI_Networking
+{
+    internal class AIInstance
+    {
+#pragma warning disable 0649
+        private NaiveAI WhiteAI;
+        private NaiveAI BlackAI;
+#pragma warning restore 0649
+
+        private static AIInstance instance;
+        internal static AIInstance Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new AIInstance();
+                return instance;
+            }
+        }
+
+        private AIInstance() { } // Make constructor private for singleton.
+
+
+        internal void Move(BackgammonGame model, CheckerColor c)
+        {
+            Move(model, c, (c == CheckerColor.White) ? WhiteAI : BlackAI);
+        }
+
+        private void Move(BackgammonGame model, CheckerColor c, NaiveAI ai)
+        {
+            if (ai == null)
+                ai = new NaiveAI(model, c);
+            ai.MakeMove();
+        }
+
+        private void NintyPercentJunk()
+        {
+
+        }
+
+    }
+}
